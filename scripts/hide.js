d2813cda9ae19302005bc10dd5f47b0d0a941bb7
Bijeet-D <154560979+Bijeet-D@users.noreply.github.com>
2025-09-27 14:02:06 -0700

    Update hide.js

--- scripts/hide.js
+++ scripts/hide.js
@@ -1,18 +1,25 @@
 const frame = document.getElementById("sign-in")
-  const frameOGZIndex = 9999
-  let keyForHide = "Alt"
-
+  let keyForHide = ["p","l"]
+  let keysPressed = [];
+    
 document.addEventListener("keydown", (event) => {
+    if (!keysPressed.includes(event.key)) {
+    keysPressed.push(event.key);
+    }
+  
   // Check which key was pressed
-  if (event.key === keyForHide) {
-    frame.style.zIndex = -1
+  if (keysPressed.includes(keyForHide[0]) && keysPressed.includes(keyForHide[1])) {
+    frame.style.zIndex = "-1";
   } 
 });
 
 document.addEventListener("keyup", (event) => {
-  if (event.key === keyForHide) {
-    frame.style.zIndex = frameOGZIndex
-  } 
+  if (keysPressed.includes(event.key)) {
+    const indexOfKey = keysPressed.indexOf(event.key)
+  keysPressed.splice(indexOfKey, 1)
+      frame.style.zIndex = "9999";
+    }
+
 });
 
 
