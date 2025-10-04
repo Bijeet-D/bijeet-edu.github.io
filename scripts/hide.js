eee6ee12236b7ec9f9717c1aeab08e3cb670b341
Bijeet-D <154560979+Bijeet-D@users.noreply.github.com>
2025-10-04 13:45:05 -0700

    Update hide.js

--- scripts/hide.js
+++ scripts/hide.js
@@ -3,15 +3,18 @@ let hideKeys = ["p", "l"]
 let KeysPressed = {};  // keys pressed object
 
 onkeydown = onkeyup = function(e) {
-  e = e || event; // fallback for old browsers idk whose acually gonna be on one but who cares
-  map[e.key] = (e.type === "keydown"); 
-  /* insert conditional here */
-};
+  e = e || event; // fallback for old browsers idk whose acually gonna be on windows 95 on internet explorer and be here but who cares cuz it was in the stack overflow code from like 5 years ago and im too lazy to remove it even though it takes longet to type this 
+  KeysPressed[e.key] = (e.type === "keydown"); 
 
-
-if (KeysPressed[hideKeys[0]] && KeysPressed[hideKeys[1]]) {
+  if (KeysPressed[hideKeys[0]] && KeysPressed[hideKeys[1]]) {
   document.body.style.visibility = 'hidden'; // hide the whole body
 } else {
   document.body.style.visibility = 'visible'; // show it again
 }
+
+  
+};
+
+
+
 
