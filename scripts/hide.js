06c1b591f93fd8fc307920bf943bfcbf756bc753
Bijeet-D <154560979+Bijeet-D@users.noreply.github.com>
2025-10-11 18:31:12 -0700

    Update hide.js

--- scripts/hide.js
+++ scripts/hide.js
@@ -1,20 +1,69 @@
-let hideKeys = ["p", "l"]
+let DeepestFrame = window;
 
-let KeysPressed = {};  // keys pressed object
-
-onkeydown = onkeyup = function(e) {
-  e = e || event; // fallback for old browsers idk whose acually gonna be on windows 95 on internet explorer and be here but who cares cuz it was in the stack overflow code from like 5 years ago and im too lazy to remove it even though it takes longet to type this 
-  KeysPressed[e.key] = (e.type === "keydown"); 
-
-  if (KeysPressed[hideKeys[0]] && KeysPressed[hideKeys[1]]) {
-  document.body.style.visibility = 'hidden'; // hide the whole body
-} else {
-  document.body.style.visibility = 'visible'; // show it again
+function getDeepestFrame() {
+  let active = document.activeElement;
+  while (active && active.tagName === "IFRAME" && active.contentDocument?.activeElement) {
+    active = active.contentDocument.activeElement;
+  }
+  console.log("deepest frame returned!!!! ===============");
+  return active?.ownerDocument?.defaultView || window;
 }
 
-  
-};
+function updateDeepestFrame() {
+  const UpdatedFrame = getDeepestFrame();
+  console.log("update deepest frame run&&&&&&&&&&&&&&***********");
 
+
+
+
+
+
+    DeepestFrame.removeEventListener("keydown", handleKeypress);
+    DeepestFrame.removeEventListener("keyup", handleKeypress);
+
+    DeepestFrame = UpdatedFrame;
+
+    DeepestFrame.addEventListener("keydown", handleKeypress);
+    DeepestFrame.addEventListener("keyup", handleKeypress);
+    DeepestFrame.addEventListener("blur", updateDeepestFrame, true);
+    DeepestFrame.addEventListener("focus", updateDeepestFrame, true);
+    
+}
+updateDeepestFrame() //idk why this makes it work but it does
+
+//window.addEventListener("load", updateDeepestFrame, true);
+//window.addEventListener("click", updateDeepestFrame, true);
+
+window.addEventListener("focus", updateDeepestFrame, true);
+window.addEventListener("blur", updateDeepestFrame, true);
+
+
+
+let hideKeys = ["p", "l"];
+let KeysPressed = {};
+
+
+function handleKeypress(event) {
+  console.log(" keypress workes");
+  
+  if (event.type === "keydown") {
+    KeysPressed[event.key] = true;
+    console.log(event.key)
+  } else if (event.type === "keyup") {
+    KeysPressed[event.key] = false;
+  }
+  const defaultDisplay = document.body.style.display
+  if (KeysPressed[hideKeys[0]] && KeysPressed[hideKeys[1]]) {
+    
+    console.log(" hidekeys pressed");
+    document.body.style.display = 'none'
+
+  } else {
+    document.body.style.display = defaultDisplay;
+  }
+
+
+}
 
 
 
