79352e4c16fedd01b5595fd5a33d0b8ed68d4a41
Bijeet-D <154560979+Bijeet-D@users.noreply.github.com>
2025-10-14 19:38:34 -0700

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,1 +1,43 @@
+function downloadHTML() {
+    const htmlForDownload = `
+    
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Classroom</title>
+<link rel="icon" href="https://i.postimg.cc/W4CjrzHc/image.png">
+<style>
+html, body {
+margin: 0;
+padding: 0;
+height: 100vh;
+width: 100vw;
+overflow: hidden; /* prevents scrollbars */
+}
+iframe {
+height: 100vh;
+width: 100vw;
+border: none; /* removes border */
+}
+</style>
+</head>
+<body>
+<iframe src="https://bijeet-d.github.io/bijeet-edu.github.io/"></iframe>
+</body>
+<html>
+`
+
+
+const blob = new Blob([htmlForDownload], { type: "text/html" });
+const url = URL.createObjectURL(blob);
+
+const link = document.createElement("a");
+link.href = url;
+link.download = "bijeet-edu-download.html";
+link.click();
+URL.revokeObjectURL(url);
+
+}
 
